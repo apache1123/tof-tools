e1146d3ea3ea491788e9318441e142fb07dada0e
apache1123 <apache1123@gmail.com>
2023-05-29 16:46:08 +1200

    Add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:storybook/recommended',
     'next/core-web-vitals',
   ],
   parser: '@typescript-eslint/parser',
