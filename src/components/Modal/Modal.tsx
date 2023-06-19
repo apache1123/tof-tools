28d8551ca4f5e13ac3520ffccacffef7dbc1a4e4
apache1123 <apache1123@gmail.com>
2023-06-19 16:46:56 +1200

    Standardize modal styles a bit

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,15 @@
+import { SxProps } from '@mui/material';
+
+export const modalStyle: SxProps = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+// Can do a custom Modal component in the future...
