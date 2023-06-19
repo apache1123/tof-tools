fa258da6ccfc4da4b3438c16f4d6bd9df0170e0f
apache1123 <apache1123@gmail.com>
2023-06-20 02:21:42 +1200

    Add OCR example screenshot

--- src/components/GearOCRModal/GearOCRModal.tsx
+++ src/components/GearOCRModal/GearOCRModal.tsx
@@ -1,14 +1,13 @@
-import { Button, Modal, Paper, Typography } from '@mui/material';
+import { Box, Button, Modal, Paper, Typography } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import Image from 'next/image';
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 
 import { useGear } from '../../hooks/useGear';
 import { Gear } from '../../models/gear';
 import { GearType } from '../../models/gear-type';
 import { gearOCRService } from '../../services/gear-ocr-service';
 import { GearPiece } from '../GearPiece/GearPiece';
-import { GridBreak } from '../GridBreak/GridBreak';
 import { ImageOCR } from '../ImageOCR/ImageOCR';
 import { modalStyle } from '../Modal/Modal';
 
@@ -51,7 +50,7 @@ export const GearOCRModal = ({
   };
 
   return (
-    <div>
+    <>
       <Button onClick={handleOpen}>Upload gear</Button>
       <Modal
         open={open}
@@ -60,7 +59,7 @@ export const GearOCRModal = ({
         aria-describedby="modal-modal-description"
       >
         <Paper sx={modalStyle} elevation={0}>
-          <Grid container spacing={3}>
+          <Grid container>
             <Grid xs></Grid>
             <Grid
               xs={12}
@@ -74,41 +73,41 @@ export const GearOCRModal = ({
                 onOCRTextChange={handleOCRTextChange}
                 onImageURLChange={handleImageURLChange}
               />
-              <Typography variant="caption">
-                Gear name & random stats should be clearly visible in the
-                screenshot
-              </Typography>
             </Grid>
             <Grid xs></Grid>
+          </Grid>
+          <Box textAlign="center" mb={3}>
+            <Typography variant="caption">
+              Gear name & random stats should be clearly visible in the
+              screenshot. <ExampleScreenshotModal />
+            </Typography>
+          </Box>
 
-            <GridBreak />
+          {imageURL && (
+            <Grid container spacing={3} mb={3}>
+              <Grid xs={12} md={6}>
+                <Image
+                  src={imageURL}
+                  width={350}
+                  height={450}
+                  alt="uploaded-image-preview"
+                />
+              </Grid>
+              <Grid xs={12} md={6}>
+                <GearPiece
+                  selectedGear={gear}
+                  possibleGearTypes={gearTypes}
+                  showGearOCRButton={false}
+                  onGearTypeChange={setGearType}
+                  onGearStarsChange={setGearStars}
+                  onRandomStatTypeChange={setRandomStatType}
+                  onRandomStatValueChange={setRandomStatValue}
+                />
+              </Grid>
+            </Grid>
+          )}
 
-            {imageURL && (
-              <Fragment>
-                <Grid xs={12} md={6}>
-                  <Image
-                    src={imageURL}
-                    width={350}
-                    height={450}
-                    alt="uploaded-image-preview"
-                  />
-                </Grid>
-                <Grid xs={12} md={6}>
-                  <GearPiece
-                    selectedGear={gear}
-                    possibleGearTypes={gearTypes}
-                    showGearOCRButton={false}
-                    onGearTypeChange={setGearType}
-                    onGearStarsChange={setGearStars}
-                    onRandomStatTypeChange={setRandomStatType}
-                    onRandomStatValueChange={setRandomStatValue}
-                  />
-                </Grid>
-              </Fragment>
-            )}
-
-            <GridBreak />
-
+          <Grid container>
             <Grid xs></Grid>
             <Grid xs={12} sm="auto">
               <Button
@@ -126,6 +125,41 @@ export const GearOCRModal = ({
           </Grid>
         </Paper>
       </Modal>
-    </div>
+    </>
   );
 };
+
+function ExampleScreenshotModal() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Button
+        onClick={handleOpen}
+        variant="text"
+        sx={{ textTransform: 'Initial', p: 0 }}
+      >
+        Example
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Paper sx={modalStyle} elevation={0}>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Image
+              src="/example_screenshot.png"
+              alt="example-screenshot"
+              width={350}
+              height={450}
+            />
+          </Box>
+        </Paper>
+      </Modal>
+    </>
+  );
+}
