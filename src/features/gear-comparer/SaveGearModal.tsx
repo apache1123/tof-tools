f4e915baf8164ef1eb612e75caab047c632f4059
apache1123 <apache1123@gmail.com>
2023-07-10 17:22:32 +1200

    Create snackbar component

--- src/features/gear-comparer/SaveGearModal.tsx
+++ src/features/gear-comparer/SaveGearModal.tsx
@@ -3,6 +3,7 @@ import { Box } from '@mui/material';
 import { useState } from 'react';
 
 import { ButtonModal } from '../../components/ButtonModal/ButtonModal';
+import { useAutoHideSnackbar } from '../../components/Snackbar/useAutoHideSnackbar';
 import type { Gear } from '../../models/gear';
 import { copyGear } from '../../models/gear';
 import type { GearSet } from '../../models/gear-set';
@@ -15,28 +16,41 @@ export interface SaveGearModalProps {
 export function SaveGearModal({ gear }: SaveGearModalProps) {
   const [selectedGearSet, setSelectedGearSet] = useState<GearSet>();
 
+  const { SnackbarComponent, showSnackbar } = useAutoHideSnackbar({});
+
   return (
-    <ButtonModal
-      buttonText="Save gear to gear set"
-      modalContent={
-        <>
-          <Box mb={1}>Save gear to gear set</Box>
-          <GearSetSelector
-            onGearSetSelect={(gearSet) => setSelectedGearSet(gearSet)}
-          />
-        </>
-      }
-      showConfirm
-      showCancel
-      onConfirm={() => {
-        if (selectedGearSet) {
-          const replaceeGear = selectedGearSet.gearsByTypeId[gear.typeId];
-          if (replaceeGear) {
-            copyGear(gear, replaceeGear);
-          }
+    <>
+      <ButtonModal
+        buttonText="Save gear to gear set"
+        modalContent={
+          <>
+            <Box mb={1}>Save gear to gear set</Box>
+            <GearSetSelector
+              onGearSetSelect={(gearSet) => setSelectedGearSet(gearSet)}
+            />
+          </>
         }
-      }}
-      iconButtonIcon={<SaveIcon />}
-    />
+        showConfirm
+        showCancel
+        onConfirm={() => {
+          if (selectedGearSet) {
+            const replaceeGear = selectedGearSet.gearsByTypeId[gear.typeId];
+            if (replaceeGear) {
+              copyGear(gear, replaceeGear);
+
+              if (selectedGearSet.name) {
+                showSnackbar(
+                  `Gear saved to "${selectedGearSet.name}" gear set`
+                );
+              } else {
+                showSnackbar('Gear saved to gear set');
+              }
+            }
+          }
+        }}
+        iconButtonIcon={<SaveIcon />}
+      />
+      <SnackbarComponent />
+    </>
   );
 }
