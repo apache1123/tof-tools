296e0fddbd980af4c7ec91c0ef40b165c401e7b3
apache1123 <apache1123@gmail.com>
2023-07-19 01:46:36 +1200

    Support titan gear prefix OCR parsing

--- src/constants/gear.ts
+++ src/constants/gear.ts
@@ -3,4 +3,5 @@ export const maxNumOfRandomStatRolls = 5;
 export const defaultNumOfRandomStats = 4;
 
 export const goldGearNamePrefix = 'Fortress';
+export const titanGearNamePrefix = 'Titan';
 export const randomStatsSectionTitle = 'Random Stats';
