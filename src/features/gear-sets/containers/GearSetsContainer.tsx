ba2e7c76966a62416af8ce296c834978a05b93fb
apache1123 <apache1123@gmail.com>
2023-07-20 18:37:52 +1200

    Also initialize ocr worker on gear set page

--- src/features/gear-sets/containers/GearSetsContainer.tsx
+++ src/features/gear-sets/containers/GearSetsContainer.tsx
@@ -1,11 +1,17 @@
 import { Box, Paper, Stack } from '@mui/material';
+import { useEffect } from 'react';
 
+import { initializeOCRWorker } from '../../../stores/ocr-temp-gear';
 import { CurrentGearSetGears } from '../CurrentGearSetGears';
 import { CurrentGearSetName } from '../CurrentGearSetName';
 import { DeleteCurrentGearSet } from '../DeleteCurrentGearSet';
 import { GearSetTabs } from '../GearSetTabs';
 
 export function GearSetsContainer() {
+  useEffect(() => {
+    initializeOCRWorker();
+  }, []);
+
   return (
     <>
       <GearSetTabs />
