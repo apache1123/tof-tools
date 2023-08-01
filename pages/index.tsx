5fc0ffd436540a8ac9b90bd986c5437685074aa9
apache1123 <apache1123@gmail.com>
2023-08-01 15:28:57 +1200

    Add sentry

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material';
 import Container from '@mui/material/Container';
 import Head from 'next/head';
 
@@ -8,7 +9,15 @@ export default function Home() {
         <title>ToF tools</title>
       </Head>
 
-      <Container maxWidth="lg" sx={{ p: 3 }}></Container>
+      <Container maxWidth="lg" sx={{ p: 3 }}>
+        <Button
+          onClick={() => {
+            throw new Error('test');
+          }}
+        >
+          Test error
+        </Button>
+      </Container>
     </>
   );
 }
