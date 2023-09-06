4e51b8c1931619362b2a563f14f4a8fc358448b3
apache1123 <apache1123@gmail.com>
2023-09-07 00:18:00 +1200

    Update pull-up logic (#67)
    * Update pull-up logic

--- src/constants/gear.ts
+++ src/constants/gear.ts
@@ -2,7 +2,9 @@ export const maxNumOfRandomStatRolls = 5;
 export const defaultNumOfRandomStats = 4;
 export const maxNumOfAugmentStats = 2;
 
-export const augmentStatsPullUpFactor = 0.95;
+export const augmentStatsPullUpFactor1 = 0.95;
+export const augmentStatsPullUpFactor2 = 0.9;
+export const augmentStatsPullUpFactor3 = 0.85; // unconfirmed
 
 export const goldGearNamePrefix = 'Fortress';
 export const titanGearNamePrefix = 'Titan';
