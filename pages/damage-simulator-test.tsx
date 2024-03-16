d3f96f2f599cb8aecf642ac81992598f4ce2bf3c
apache1123 <apache1123@gmail.com>
2024-03-16 21:59:22 +1300

    Add temp damage simulator page

---
+++ pages/damage-simulator-test.tsx
@@ -0,0 +1,22 @@
+import { Container } from '@mui/material';
+import Head from 'next/head';
+
+import { CombatSimulatorTimeline } from '../src/features/combat-simulator/CombatSimulatorTimeline';
+import { DamageSimulator } from '../src/features/damage-simulator/DamageSimulator';
+import { Relics } from '../src/features/relics/Relics';
+
+export default function DamageSimulatorTestPage() {
+  return (
+    <>
+      <Head>
+        <title>Damage Simulator | Tower of Fantasy Tools</title>
+      </Head>
+
+      <Container maxWidth="lg" sx={{ p: 3 }}>
+        <CombatSimulatorTimeline />
+        <DamageSimulator />
+        <Relics />
+      </Container>
+    </>
+  );
+}
--- pages/damage-simulator.tsx
+++ pages/damage-simulator.tsx
@@ -1,9 +1,6 @@
-import { Container } from '@mui/material';
+import { Box, Container, Paper, Typography } from '@mui/material';
 import Head from 'next/head';
-
-import { CombatSimulatorTimeline } from '../src/features/combat-simulator/CombatSimulatorTimeline';
-import { DamageSimulator } from '../src/features/damage-simulator/DamageSimulator';
-import { Relics } from '../src/features/relics/Relics';
+import Image from 'next/image';
 
 export default function DamageSimulatorPage() {
   return (
@@ -13,9 +10,19 @@ export default function DamageSimulatorPage() {
       </Head>
 
       <Container maxWidth="lg" sx={{ p: 3 }}>
-        <CombatSimulatorTimeline />
-        <DamageSimulator />
-        <Relics />
+        <Paper sx={{ p: 2, textAlign: 'center', mb: 10 }}>
+          <Box mb={10}>
+            <Image
+              src="/stickers/赦免天使在工作 01.gif"
+              alt="Under construction"
+              width={240}
+              height={240}
+            />
+          </Box>
+          <Typography variant="h4" component="h2" mb={5}>
+            Under construction. Coming soon!
+          </Typography>
+        </Paper>
       </Container>
     </>
   );
