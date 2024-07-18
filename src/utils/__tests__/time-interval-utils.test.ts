3d3b1dbbaf9a896228b2ddffbfbfd20bbedab02d
apache1123 <apache1123@gmail.com>
2024-07-18 17:09:39 +1200

    TimeInterval util tests

--- src/utils/__tests__/time-interval-utils.test.ts
+++ src/utils/__tests__/time-interval-utils.test.ts
@@ -1,32 +1,36 @@
-import { calculateOverlapDuration } from '../time-interval-utils';
+import { TimeInterval } from '../../models/v4/time-interval/time-interval';
+import {
+  calculateOverlapDuration,
+  getLatestTimeInterval,
+} from '../time-interval-utils';
 
 describe('calculateOverlapDuration', () => {
   it("returns 0 if the two intervals don't overlap", () => {
     expect(
       calculateOverlapDuration(
-        { startTime: 10, endTime: 20 },
-        { startTime: 40, endTime: 50 }
+        new TimeInterval(10, 20),
+        new TimeInterval(40, 50)
       )
     ).toBe(0);
 
     expect(
       calculateOverlapDuration(
-        { startTime: 40, endTime: 50 },
-        { startTime: 10, endTime: 20 }
+        new TimeInterval(40, 50),
+        new TimeInterval(10, 20)
       )
     ).toBe(0);
 
     expect(
       calculateOverlapDuration(
-        { startTime: 10, endTime: 20 },
-        { startTime: 20, endTime: 50 }
+        new TimeInterval(10, 20),
+        new TimeInterval(20, 50)
       )
     ).toBe(0);
 
     expect(
       calculateOverlapDuration(
-        { startTime: 20, endTime: 50 },
-        { startTime: 10, endTime: 20 }
+        new TimeInterval(20, 50),
+        new TimeInterval(10, 20)
       )
     ).toBe(0);
   });
@@ -34,37 +38,65 @@ describe('calculateOverlapDuration', () => {
   it('returns the correct duration of overlap', () => {
     expect(
       calculateOverlapDuration(
-        { startTime: 10, endTime: 20 },
-        { startTime: 0, endTime: 12 }
+        new TimeInterval(10, 20),
+        new TimeInterval(0, 12)
       )
     ).toBe(2);
 
     expect(
       calculateOverlapDuration(
-        { startTime: 10, endTime: 20 },
-        { startTime: 13, endTime: 17 }
+        new TimeInterval(10, 20),
+        new TimeInterval(13, 17)
       )
     ).toBe(4);
 
     expect(
       calculateOverlapDuration(
-        { startTime: 10, endTime: 20 },
-        { startTime: 13, endTime: 30 }
+        new TimeInterval(10, 20),
+        new TimeInterval(13, 30)
       )
     ).toBe(7);
 
     expect(
       calculateOverlapDuration(
-        { startTime: 10, endTime: 20 },
-        { startTime: 0, endTime: 40 }
+        new TimeInterval(10, 20),
+        new TimeInterval(0, 40)
       )
     ).toBe(10);
 
     expect(
       calculateOverlapDuration(
-        { startTime: 0, endTime: 40 },
-        { startTime: 10, endTime: 20 }
+        new TimeInterval(0, 40),
+        new TimeInterval(10, 20)
       )
     ).toBe(10);
+  });
+});
+
+describe('getLatestTimeInterval', () => {
+  it('returns the latest interval from an array of intervals', () => {
+    expect(
+      getLatestTimeInterval([
+        new TimeInterval(10, 20),
+        new TimeInterval(40, 50),
+        new TimeInterval(0, 12),
+      ])
+    ).toEqual(new TimeInterval(40, 50));
+  });
+
+  it('returns the latest interval that is closest to the end of the array if there are multiple with the latest end time', () => {
+    expect(
+      getLatestTimeInterval([
+        new TimeInterval(10, 20),
+        new TimeInterval(40, 50),
+        new TimeInterval(30, 50),
+        new TimeInterval(0, 12),
+        new TimeInterval(20, 50),
+      ])
+    ).toEqual(new TimeInterval(20, 50));
+  });
+
+  it('throws if the array is empty', () => {
+    expect(() => getLatestTimeInterval([])).toThrow();
   });
 });
