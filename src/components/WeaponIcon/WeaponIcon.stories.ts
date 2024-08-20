f195bc0b06880a12338015bd2aa3140ff31b2977
apache1123 <apache1123@gmail.com>
2024-08-20 22:40:24 +1200

    Support overlaying elemental icon on top of weapon icon

---
+++ src/components/WeaponIcon/WeaponIcon.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { WeaponIcon } from './WeaponIcon';
+
+const meta: Meta<typeof WeaponIcon> = {
+  title: 'Weapon Icon',
+  component: WeaponIcon,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof WeaponIcon>;
+
+export const Initial: Story = {
+  args: {
+    weaponName: 'Nola',
+    size: 100,
+    elementalIcon: 'Flame-Physical',
+  },
+};
