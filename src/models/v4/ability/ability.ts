cc1e8d887a5a65eff883aebc4be2c1c10b8d7afc
apache1123 <apache1123@gmail.com>
2024-09-17 20:18:01 +1200

    Ability tests

--- src/models/v4/ability/ability.ts
+++ src/models/v4/ability/ability.ts
@@ -3,7 +3,7 @@ import type { CurrentCombatState } from '../combat-state/current-combat-state';
 import type { EventManager } from '../event/event-manager';
 import type { CurrentTick } from '../tick/current-tick';
 import { TimeInterval } from '../time-interval/time-interval';
-import type { AbilityEvent } from './ability-event';
+import { AbilityEvent } from './ability-event';
 import type { AbilityId } from './ability-id';
 import type { AbilityRequirements } from './ability-requirements';
 import type { AbilityTimeline } from './ability-timeline';
@@ -11,7 +11,7 @@ import type { AbilityUpdatesResource } from './ability-updates-resource';
 import type { AbilityDto } from './dtos/ability-dto';
 
 /** An ability is anything a character does. Attacks, buffs etc. are all considered abilities. */
-export class Ability<TAbilityEvent extends AbilityEvent = AbilityEvent>
+export abstract class Ability<TAbilityEvent extends AbilityEvent = AbilityEvent>
   implements Serializable<AbilityDto>
 {
   public constructor(
@@ -47,15 +47,21 @@ export class Ability<TAbilityEvent extends AbilityEvent = AbilityEvent>
     return this.canBePlayerTriggered && this.canTrigger();
   }
 
+  
   /** Trigger an ability event */
   public trigger() {
     if (!this.canTrigger()) return;
-
+    
     const newEventTimeInterval = this.getNewEventTimeInterval();
     const newEvent = this.createNewEvent(newEventTimeInterval);
     this.timeline.addEvent(newEvent);
-
+    
     this.eventManager.publishAbilityStarted({ id: this.id });
+  }
+
+  /** Has ongoing events */
+  public isOngoing() {
+    return this.getOngoingEvents().length > 0;
   }
 
   /** Perform whatever actions needed during the current tick for this ability. Emit events, update resources, etc. */
@@ -72,19 +78,11 @@ export class Ability<TAbilityEvent extends AbilityEvent = AbilityEvent>
     }
   }
 
-  /** Has ongoing events */
-  public isOngoing() {
-    return this.getOngoingEvents().length > 0;
-  }
-
   protected getOngoingEvents(): TAbilityEvent[] {
     return this.timeline.getEventsOverlapping(this.currentTick.value);
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  protected createNewEvent(timeInterval: TimeInterval): TAbilityEvent {
-    throw new Error('Not implemented');
-  }
+  protected abstract createNewEvent(timeInterval: TimeInterval): TAbilityEvent;
 
   /** Terminate any active ability events */
   private terminate() {
@@ -107,5 +105,20 @@ export class Ability<TAbilityEvent extends AbilityEvent = AbilityEvent>
   public toDto(): AbilityDto {
     const { id, displayName, timeline } = this;
     return { id, displayName, timeline: timeline.toDto(), version: 1 };
+  }
+}
+
+/** A concrete class that extends the abstract class to aid testing */
+export class ConcreteAbility extends Ability<AbilityEvent> {
+  protected override createNewEvent(timeInterval: TimeInterval): AbilityEvent {
+    return new AbilityEvent(
+      timeInterval,
+      this.id,
+      this.cooldown,
+      this.updatesResources,
+      this.eventManager,
+      this.currentTick,
+      this.currentCombatState
+    );
   }
 }
