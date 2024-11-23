084a9496226490a9f752b10937cf07f69ad3f2ac
apache1123 <apache1123@gmail.com>
2024-11-23 21:41:45 +1300

    Add catppuccin colors to theme var to access directly

--- src/theme.ts
+++ src/theme.ts
@@ -129,6 +129,84 @@ theme = createTheme(theme, {
         main: pink[300],
       },
     }),
+    // Catppuccin colors
+    rosewater: theme.palette.augmentColor({
+      color: { main: colors.rosewater.hex },
+    }),
+    flamingo: theme.palette.augmentColor({
+      color: { main: colors.flamingo.hex },
+    }),
+    pink: theme.palette.augmentColor({
+      color: { main: colors.pink.hex },
+    }),
+    mauve: theme.palette.augmentColor({
+      color: { main: colors.mauve.hex },
+    }),
+    red: theme.palette.augmentColor({
+      color: { main: colors.red.hex },
+    }),
+    maroon: theme.palette.augmentColor({
+      color: { main: colors.maroon.hex },
+    }),
+    peach: theme.palette.augmentColor({
+      color: { main: colors.peach.hex },
+    }),
+    yellow: theme.palette.augmentColor({
+      color: { main: colors.yellow.hex },
+    }),
+    green: theme.palette.augmentColor({
+      color: { main: colors.green.hex },
+    }),
+    teal: theme.palette.augmentColor({
+      color: { main: colors.teal.hex },
+    }),
+    sky: theme.palette.augmentColor({
+      color: { main: colors.sky.hex },
+    }),
+    sapphire: theme.palette.augmentColor({
+      color: { main: colors.sapphire.hex },
+    }),
+    blue: theme.palette.augmentColor({
+      color: { main: colors.blue.hex },
+    }),
+    lavender: theme.palette.augmentColor({
+      color: { main: colors.lavender.hex },
+    }),
+    // Catppuccin text
+    subtext1: theme.palette.augmentColor({
+      color: { main: colors.subtext1.hex },
+    }),
+    subtext0: theme.palette.augmentColor({
+      color: { main: colors.subtext0.hex },
+    }),
+    // Catppuccin surfaces
+    overlay2: theme.palette.augmentColor({
+      color: { main: colors.overlay2.hex },
+    }),
+    overlay1: theme.palette.augmentColor({
+      color: { main: colors.overlay1.hex },
+    }),
+    overlay0: theme.palette.augmentColor({
+      color: { main: colors.overlay0.hex },
+    }),
+    surface2: theme.palette.augmentColor({
+      color: { main: colors.surface2.hex },
+    }),
+    surface1: theme.palette.augmentColor({
+      color: { main: colors.surface1.hex },
+    }),
+    surface0: theme.palette.augmentColor({
+      color: { main: colors.surface0.hex },
+    }),
+    base: theme.palette.augmentColor({
+      color: { main: colors.base.hex },
+    }),
+    mantle: theme.palette.augmentColor({
+      color: { main: colors.mantle.hex },
+    }),
+    crust: theme.palette.augmentColor({
+      color: { main: colors.crust.hex },
+    }),
   },
 });
 
@@ -140,6 +218,34 @@ declare module "@mui/material/styles" {
     physical: PaletteOptions["primary"];
     volt: PaletteOptions["primary"];
     titan: PaletteOptions["primary"];
+    // Catppuccin colors
+    rosewater: PaletteOptions["primary"];
+    flamingo: PaletteOptions["primary"];
+    pink: PaletteOptions["primary"];
+    mauve: PaletteOptions["primary"];
+    red: PaletteOptions["primary"];
+    maroon: PaletteOptions["primary"];
+    peach: PaletteOptions["primary"];
+    yellow: PaletteOptions["primary"];
+    green: PaletteOptions["primary"];
+    teal: PaletteOptions["primary"];
+    sky: PaletteOptions["primary"];
+    sapphire: PaletteOptions["primary"];
+    blue: PaletteOptions["primary"];
+    lavender: PaletteOptions["primary"];
+    // Catppuccin text
+    subtext1: PaletteOptions["primary"];
+    subtext0: PaletteOptions["primary"];
+    // Catppuccin surfaces
+    overlay2: PaletteOptions["primary"];
+    overlay1: PaletteOptions["primary"];
+    overlay0: PaletteOptions["primary"];
+    surface2: PaletteOptions["primary"];
+    surface1: PaletteOptions["primary"];
+    surface0: PaletteOptions["primary"];
+    base: PaletteOptions["primary"];
+    mantle: PaletteOptions["primary"];
+    crust: PaletteOptions["primary"];
   }
 
   interface Palette {
@@ -149,6 +255,34 @@ declare module "@mui/material/styles" {
     physical: Palette["primary"];
     volt: Palette["primary"];
     titan: Palette["primary"];
+    // Catppuccin colors
+    rosewater: Palette["primary"];
+    flamingo: Palette["primary"];
+    pink: Palette["primary"];
+    mauve: Palette["primary"];
+    red: Palette["primary"];
+    maroon: Palette["primary"];
+    peach: Palette["primary"];
+    yellow: Palette["primary"];
+    green: Palette["primary"];
+    teal: Palette["primary"];
+    sky: Palette["primary"];
+    sapphire: Palette["primary"];
+    blue: Palette["primary"];
+    lavender: Palette["primary"];
+    // Catppuccin text
+    subtext1: Palette["primary"];
+    subtext0: Palette["primary"];
+    // Catppuccin surfaces
+    overlay2: Palette["primary"];
+    overlay1: Palette["primary"];
+    overlay0: Palette["primary"];
+    surface2: Palette["primary"];
+    surface1: Palette["primary"];
+    surface0: Palette["primary"];
+    base: Palette["primary"];
+    mantle: Palette["primary"];
+    crust: Palette["primary"];
   }
 }
 
