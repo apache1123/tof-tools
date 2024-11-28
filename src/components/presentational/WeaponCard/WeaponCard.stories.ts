1a34dbf0765348d9d30ba437cc1e2e8e3f57c6b2
apache1123 <apache1123@gmail.com>
2024-11-28 20:22:34 +1300

    Weapon card

---
+++ src/components/presentational/WeaponCard/WeaponCard.stories.ts
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { weaponDefinitions } from "../../../definitions/weapons/weapon-definitions";
+import { Weapon } from "../../../models/weapon/weapon";
+import { WeaponCard } from "./WeaponCard";
+
+const meta: Meta<typeof WeaponCard> = {
+  title: "Weapon Card",
+  component: WeaponCard,
+  tags: ["autodocs"],
+};
+export default meta;
+
+type Story = StoryObj<typeof WeaponCard>;
+
+const weapon = new Weapon(weaponDefinitions.byId["King"], "characterId");
+export const Default: Story = {
+  args: { weapon },
+};
---
+++ src/components/presentational/WeaponCard/WeaponCard.tsx
@@ -0,0 +1,45 @@
+import { Card, CardActionArea, CardContent, Stack } from "@mui/material";
+
+import type { Weapon } from "../../../models/weapon/weapon";
+import { WeaponDefinitionCardContent } from "../WeaponDefinitionCard/WeaponDefinitionCardContent";
+import { WeaponStarsSelector } from "../WeaponStarsSelector/WeaponStarsSelector";
+
+export interface WeaponCardProps {
+  weapon: Weapon;
+  onClick?(weapon: Weapon): void;
+}
+
+export function WeaponCard({ weapon, onClick }: WeaponCardProps) {
+  const {
+    id,
+    weaponDisplayName,
+    simulacrumDisplayName,
+    elementalIcon,
+    type,
+    stars,
+  } = weapon;
+
+  return (
+    <Card sx={{ width: "fit-content" }}>
+      <CardActionArea
+        onClick={() => {
+          if (onClick) onClick(weapon);
+        }}
+        sx={{ height: "100%", display: "flex", alignItems: "flex-start" }}
+      >
+        <CardContent sx={{ p: 0 }}>
+          <Stack sx={{ gap: 2, alignItems: "center", pb: 2 }}>
+            <WeaponDefinitionCardContent
+              id={id}
+              weaponDisplayName={weaponDisplayName}
+              simulacrumDisplayName={simulacrumDisplayName}
+              elementalIcon={elementalIcon}
+              type={type}
+            />
+            <WeaponStarsSelector stars={stars} readOnly />
+          </Stack>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
