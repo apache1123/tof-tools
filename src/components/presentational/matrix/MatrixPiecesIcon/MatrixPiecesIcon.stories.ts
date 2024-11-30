e1f6b9c4eca1663e11554502746eeb8a83709e72
apache1123 <apache1123@gmail.com>
2024-11-30 21:04:23 +1300

    Matrix pieces icon

---
+++ src/components/presentational/matrix/MatrixPiecesIcon/MatrixPiecesIcon.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { getMatrixDefinition } from "../../../../definitions/matrices/matrix-definitions";
+import { MatrixPiecesIcon } from "./MatrixPiecesIcon";
+
+const meta: Meta<typeof MatrixPiecesIcon> = {
+  title: "Matrix Pieces Icon",
+  component: MatrixPiecesIcon,
+  tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof MatrixPiecesIcon>;
+
+const { id, displayName } = getMatrixDefinition("Alyss");
+export const Default: Story = {
+  args: {
+    definitionId: id,
+    displayName,
+    pieces: 4,
+  },
+};
