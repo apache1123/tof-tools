89d6b634f853727ababc60abf1707513f1db907a
apache1123 <apache1123@gmail.com>
2025-02-15 12:37:18 +1300

    Fix div within p hydration warning

--- src/features/gear/GearRollBreakdown.tsx
+++ src/features/gear/GearRollBreakdown.tsx
@@ -25,22 +25,20 @@ export const GearRollBreakdown = ({ gear }: GearRollBreakdownProps) => {
           <ul>
             {x.randomStatRollCombinations.map((y) => (
               <li key={y.randomStatId}>
-                <Typography>
-                  <b>{`${y.randomStatId}: `}</b>
-                  {pluralize("roll", y.rollCombination.numberOfRolls, true)}
-                  {!!y.rollCombination.numberOfRolls && (
-                    <div>
-                      Average strength of each roll:{" "}
-                      <NumericStringPercentage
-                        value={y.rollCombination.rollStrength}
-                      />
-                      . Total weighting:{" "}
-                      <NumericStringPercentage
-                        value={y.rollCombination.totalRollWeight}
-                      />
-                    </div>
-                  )}
-                </Typography>
+                <b>{`${y.randomStatId}: `}</b>
+                {pluralize("roll", y.rollCombination.numberOfRolls, true)}
+                {!!y.rollCombination.numberOfRolls && (
+                  <Typography gutterBottom>
+                    Average strength of each roll:{" "}
+                    <NumericStringPercentage
+                      value={y.rollCombination.rollStrength}
+                    />
+                    . Total weighting:{" "}
+                    <NumericStringPercentage
+                      value={y.rollCombination.totalRollWeight}
+                    />
+                  </Typography>
+                )}
               </li>
             ))}
           </ul>
